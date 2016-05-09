8001f7548df5ee26dadf30a489919bd5d031d31f
Jeremy Foster <jeremy.foster@live.com>
2016-05-09 15:59:08 -0700

    Add ability to send name (-n) in as command line argument

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    name:"Rower 1"
+    name:"Rower"
 }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var prompt = require('prompt');
 var socket = require('socket.io-client')('http://localhost:8080');
 var config = require('./config');
+var args = require('minimist')(process.argv.slice(2));
+
+
 
 // var waterrower = require("node-waterrower/Waterrower");
 
@@ -13,6 +16,9 @@ var config = require('./config');
 // 	console.log("Heart Rate......" + waterrower.readHeartRate());    // [ bpm ]
 // 	console.log("Calories..." + waterrower.readCalories());    // [ kcal ]
 // }
+
+
+if(args.n) config.name = args.n;
 
 sendData();
 
